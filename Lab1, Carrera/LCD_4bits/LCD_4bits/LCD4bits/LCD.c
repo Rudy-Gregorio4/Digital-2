82186f3db80b7803b092eee4568aaa80dfa22637
Rudy-Gregorio4 <158622505+Rudy-Gregorio4@users.noreply.github.com>
2025-01-22 23:57:52 -0600

    Add files via upload

---
+++ Lab1, Carrera/LCD_4bits/LCD_4bits/LCD4bits/LCD.c
@@ -0,0 +1,8 @@
+/*
+ * CFile1.c
+ *
+ * Created: 7/16/2024 5:16:35 PM
+ *  Author: Rudy Gregorio
+ */ 
+#include "LCD.h"
+
---
+++ Lab1, Carrera/LCD_4bits/LCD_4bits/LCD4bits/LCD.h
@@ -0,0 +1,16 @@
+/*
+ * LCD.h
+ *
+ * Created: 7/16/2024 5:15:48 PM
+ *  Author: Rudy Gregorio
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+void initLCD4bits
+
+
+
+#endif /* LCD_H_ */
